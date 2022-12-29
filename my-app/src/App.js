8644d78f1dbd863baac4f9e354ca85dd9e853723
doizzie <d.lupeikyte@gmail.com>
2022-12-29 12:00:46 +0200

    constructing layout

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -4,7 +4,87 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+      <nav>
+        <a>Lorem ipsum</a>
+        <a>Lorem ipsum</a>
+        <a>Lorem ipsum</a>
+      </nav>
+      <div className="element">
+        <div className="mainBox">
+          <div className="box"><img src=""/></div>
+          <h1 className="title">Lorem ipsum dolor sit amet, consectetur</h1>
+          <h2 className="smallerTitle">Lorem ipsum</h2>
+          <h3 className="smallest title">Lorem ipsum dolor sit</h3>
+        </div>
+        <div className="smallerBox">
+          {/* <input>Placeholder...</input> */}
+          <div className="boxy">
+            <div className="oval"></div>
+            <div>
+              <h4>Lorem ipsum</h4>
+              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec ac facilisis tellus. Suspendisse vitae lobortis augue. Sed in ante nec metus elementum rhoncus.</p>
+            </div>
+          </div>
+          <div className="boxy">
+            <div className="oval"></div>
+            <div>
+              <h4>Lorem ipsum</h4>
+              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec ac facilisis tellus. Suspendisse vitae lobortis augue. Sed in ante nec metus elementum rhoncus.</p>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="element">
+        <div className="mainBox">
+          <div className="box"><img src=""/></div>
+          <h1 className="title">Lorem ipsum dolor sit amet, consectetur</h1>
+          <h2 className="smallerTitle">Lorem ipsum</h2>
+          <h3 className="smallest title">Lorem ipsum dolor sit</h3>
+        </div>
+        <div className="smallerBox">
+          {/* <input>Placeholder...</input> */}
+          <div className="boxy">
+            <div className="oval"></div>
+            <div>
+              <h4>Lorem ipsum</h4>
+              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec ac facilisis tellus. Suspendisse vitae lobortis augue. Sed in ante nec metus elementum rhoncus.</p>
+            </div>
+          </div>
+          <div className="boxy">
+            <div className="oval"></div>
+            <div>
+              <h4>Lorem ipsum</h4>
+              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec ac facilisis tellus. Suspendisse vitae lobortis augue. Sed in ante nec metus elementum rhoncus.</p>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="element">
+        <div className="mainBox">
+          <div className="box"><img src=""/></div>
+          <h1 className="title">Lorem ipsum dolor sit amet, consectetur</h1>
+          <h2 className="smallerTitle">Lorem ipsum</h2>
+          <h3 className="smallest title">Lorem ipsum dolor sit</h3>
+        </div>
+        <div className="smallerBox">
+          {/* <input>Placeholder...</input> */}
+          <div className="boxy">
+            <div className="oval"></div>
+            <div>
+              <h4>Lorem ipsum</h4>
+              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec ac facilisis tellus. Suspendisse vitae lobortis augue. Sed in ante nec metus elementum rhoncus.</p>
+            </div>
+          </div>
+          <div className="boxy">
+            <div className="oval"></div>
+            <div>
+              <h4>Lorem ipsum</h4>
+              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec ac facilisis tellus. Suspendisse vitae lobortis augue. Sed in ante nec metus elementum rhoncus.</p>
+            </div>
+          </div>
+        </div>
+      </div>
+      {/* <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
@@ -17,7 +97,7 @@ function App() {
         >
           Learn React
         </a>
-      </header>
+      </header> */}
     </div>
   );
 }
